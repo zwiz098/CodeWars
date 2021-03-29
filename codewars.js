44c9601b8fa10f66a92c339d1527ae5568b61d47
Zuri Walters <walterszuri02@gmail.com>
2021-03-28 21:33:23 -0400

    Update

--- codewars.js
+++ codewars.js
@@ -16,3 +16,49 @@ function makeNegative(num) {
 
 //Test:
 Test.assertEquals(makeNegative(42), -42);
+
+
+
+
+//Instructions:
+//There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+//Code:
+function queueTime(customers, n) {
+  var bag = new Array(n).fill(0);
+  for (let t of customers) {
+    let idx = bag.indexOf(Math.min(...bag));
+    bag[idx] += t;
+
+  }
+  return Math.max(...bag);
+}
+
+//Test:
+Test.assertEquals(queueTime([], 1), 0);
+Test.assertEquals(queueTime([1,2,3,4], 1), 10);
+Test.assertEquals(queueTime([2,2,3,3,4,4], 2), 9);
+Test.assertEquals(queueTime([1,2,3,4,5], 100), 5);
+
+
+
+
+//Instructions:
+//You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+// Complete the method which accepts such an array, and returns that single different number.
+// The input array will always be valid! (odd-length >= 3)
+
+//Code:
+function stray(numbers) {
+
+    let strayChar = numbers[0]
+    if (strayChar !== numbers[1] && strayChar !== numbers[2]) return strayChar
+
+    for(let i = 1; i < numbers.length; i++){
+      if(strayChar !== numbers[i])return numbers[i]
+    }
+}
+
+
+//Test:
+Test.assertEquals(stray([1, 1, 2]), 2)
