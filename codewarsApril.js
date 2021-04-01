1542e34cf0ed7f20eaa8b1672edb787e9d373f7c
Zuri Walters <walterszuri02@gmail.com>
2021-04-01 18:30:57 -0400

    update

--- codewarsApril.js
+++ codewarsApril.js
@@ -27,3 +27,24 @@ Test.describe('Sample Tests', _ => {
     Test.assertEquals(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
   });
 });
+
+
+
+
+// Instructions
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+//Solution
+  var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+  function DNAStrand(dna) {
+    return dna.replace(/./g, function(y) {
+      return pairs[y];
+    });
+  }
+
+
+//Test
+Test.assertEquals(DNAStrand("AAAA"),"TTTT","String AAAA is");
+Test.assertEquals(DNAStrand("ATTGC"),"TAACG","String ATTGC is");
+Test.assertEquals(DNAStrand("GTAT"),"CATA","String GTAT is");
