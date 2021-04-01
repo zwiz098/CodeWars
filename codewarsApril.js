22553505bd20554f4960947da3b0c4f82bb3ab52
Zuri Walters <77587592+zwiz098@users.noreply.github.com>
2021-04-01 18:33:03 -0400

    Update codewarsApril.js

--- codewarsApril.js
+++ codewarsApril.js
@@ -31,7 +31,7 @@ Test.describe('Sample Tests', _ => {
 
 
 
-// Instructions
+// Instructions 
 
 // In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
