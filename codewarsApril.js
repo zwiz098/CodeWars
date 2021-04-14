b1c9fb10e307cb194b9336faacd02c31144339bf
Zuri Walters <walterszuri02@gmail.com>
2021-04-14 12:14:41 -0400

    update

--- codewarsApril.js
+++ codewarsApril.js
@@ -31,7 +31,7 @@ Test.describe('Sample Tests', _ => {
 
 
 
-// Instructions 
+// Instructions
 
 // In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
@@ -74,4 +74,31 @@ function twoSort(s) {
 describe("Basic tests",_=>{
 Test.assertEquals(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
 Test.assertEquals(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e');
+});
+
+
+//Instructions
+
+//You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog) if it is possible and false (0 in Prolog) if not. The input values are always positive.
+
+//Solution
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  if(fuelLeft * mpg >= distanceToPump ){
+    return true
+  }else{
+    return false
+  }
+};
+
+
+//Tests
+
+const assert = require("chai").assert;
+
+describe("zeroFill", function() {
+  it("Sample Tests", function() {
+    assert.equal(zeroFuel(50, 25, 2), true);
+    assert.equal(zeroFuel(100, 50, 1), false);
+  });
 });
